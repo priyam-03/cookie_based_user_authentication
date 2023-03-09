cd8ea18ae1c0a8029bae2fa7d39f71690488e70d
priyam <priyam.saha2003@gmail.com>
2023-03-09 21:50:21 +0530

    third commit

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ const user = require("./routes/userRoute");
 
 app.use("/api/v1", user);
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "./frontend/build")));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
 });
 
 // Middleware for Errors
